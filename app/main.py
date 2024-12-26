6c331c8c9f5ea0e9b53241581d438aef77bde6bd
EvenMoreH <hubert.lewan@gmail.com>
2024-12-26 13:23:04 +0100

    moved to stylesheets, centered content, enhanced scaling, added favicon

--- app/main.py
+++ app/main.py
@@ -1,81 +1,82 @@
 from fasthtml.common import * # type: ignore
 from fasthtml.common import (
-    Form, Fieldset, Label, Input, Button, Html, Head, Body, Div, P, Title, Titled, A
+    Form, Fieldset, Label, Input, Button, Html, Head, Body, Div, P, Title, Titled, A, Link
 )
 
 import re
 
-app, rt = fast_app() # type: ignore
+app, rt = fast_app(static_path="app/static") # type: ignore
+
 
 temperature_form = Form(
     method="post",
     action="/convert"
     )(
     Fieldset(
-        Label("Temperature", Input(
+        Label("Temperature:", Input(
             name="temperature",
             type="text",
             pattern="^-?\d+(\.\d+)?$",
             title="\nEnter a valid floating-point number",
             required=True,
-            style="font-size: clamp(16px, 2vw + 0.5rem, 24px);"
+            cls="inputField"
             )
         ),
-        style="padding: 10px; font-variant-caps: all-petite-caps; font-size: clamp(36px, 3vw + 0.75rem, 48px); line-height: 1.2;"
+        cls="mainLabel"
     ),
     Div(
         Button("Kelvin -> Celsius",
             name="conversion",
             value="kc",
             type="submit",
-            style="font-size: clamp(14px, 1.5vw + 0.5rem, 24px); font-weight: 500;"
+            cls="button"
         ),
-        style="padding: 10px;"
+        cls="div"
     ),
     Div(
         Button("Kelvin -> Fahrenheit",
             name="conversion",
             value="kf",
             type="submit",
-            style="font-size: clamp(14px, 1.5vw + 0.5rem, 24px); font-weight: 500;"
+            cls="button"
         ),
-        style="padding: 10px;"
+        cls="div"
     ),
     Div(
         Button("Fahrenheit -> Celsius",
             name="conversion",
             value="fc",
             type="submit",
-            style="font-size: clamp(14px, 1.5vw + 0.5rem, 24px); font-weight: 500;"
+            cls="button"
         ),
-        style="padding: 10px;"
+        cls="div"
     ),
     Div(
         Button("Fahrenheit -> Kelvin",
             name="conversion",
             value="fk",
             type="submit",
-            style="font-size: clamp(14px, 1.5vw + 0.5rem, 24px); font-weight: 500;"
+            cls="button"
         ),
-        style="padding: 10px;"
+        cls="div"
     ),
     Div(
         Button("Celsius -> Fahrenheit",
             name="conversion",
             value="cf",
             type="submit",
-            style="font-size: clamp(14px, 1.5vw + 0.5rem, 24px); font-weight: 500;"
+            cls="button"
         ),
-        style="padding: 10px;"
+        cls="div"
     ),
     Div(
         Button("Celsius -> Kelvin",
             name="conversion",
             value="ck",
             type="submit",
-            style="font-size: clamp(14px, 1.5vw + 0.5rem, 24px); font-weight: 500;"
+            cls="button"
         ),
-        style="padding: 10px;"
+        cls="div"
     ),
 )
 
@@ -83,7 +84,10 @@ temperature_form = Form(
 def homepage():
     return Html(
         Head(
-            Title("Temperature Converter")
+            Title("Temperature Converter"),
+            Link(rel="stylesheet", href="styles.css"),
+            Link(rel="icon", href="/images/favicon.ico", type="image/x-icon"),
+            Link(rel="icon", href="/images/favicon.png", type="image/png"),
         ),
         Body(
             Div(
@@ -98,16 +102,19 @@ def convert_temperature(temperature:str, conversion:str):
     if not re.match(r"^-?\d+(\.\d+)?$", temperature):
         return Html(
             Head(
-                Title("Error")
+                Title("Error"),
+                Link(rel="stylesheet", href="styles.css"),
+                Link(rel="icon", href="/images/favicon.ico", type="image/x-icon"),
+                Link(rel="icon", href="/images/favicon.png", type="image/png"),
             ),
             Body(
                 Titled("Invalid Input"),
                 P("Please enter a valid floating-point number for the temperature."),
                 Button(
                     A("Return to Form", href="/"),
-                    style="font-size: clamp(14px, 1.5vw + 0.5rem, 24px); font-weight: 500;"
+                    cls="label"
                 ),
-                style="padding: 10px"
+                cls="div"
             )
         )
 
@@ -137,22 +144,25 @@ def convert_temperature(temperature:str, conversion:str):
 
     return Html(
         Head(
-            Title("Conversion Results")
+            Title("Conversion Results"),
+            Link(rel="stylesheet", href="styles.css"),
+            Link(rel="icon", href="/images/favicon.ico", type="image/x-icon"),
+            Link(rel="icon", href="/images/favicon.png", type="image/png"),
         ),
         Body(
             Titled(
                 "Conversion Results",
-                style="font-variant-caps: all-petite-caps; font-size: clamp(24px, 3vw + 0.75rem, 24px);"
+                cls="label"
             ),
             P(
                 result,
-                style="font-size: clamp(14px, 1.5vw + 0.5rem, 24px); font-weight: 500;"
+                cls="label"
             ),
             Button(
                 A("Return to Form", href="/"),
-                style="font-size: clamp(14px, 1.5vw + 0.5rem, 24px); font-weight: 500;"
+                cls="button"
             ),
-            style="padding: 10px"
+            cls="div"
         )
     )
 
